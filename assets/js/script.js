5bb06782eed4bd561a0c14f8f102d927088f1049
MsCrieky <>
2023-10-10 15:50:54 +0000

    Add function structure to script

---
+++ assets/js/script.js
@@ -0,0 +1,31 @@
+function rungame() {
+
+}
+
+function checkAnswer() (
+
+)
+
+function calculateCorrectAnswer() {
+
+}
+
+function incrementScore() {
+
+}
+
+function incrementWrongAnnwer() {
+
+}
+
+function displayAdditionQuestion() {
+
+}
+
+function displaySubtractQuestion() {
+
+}
+
+function displayMultiplyQuestion() {
+    
+}
