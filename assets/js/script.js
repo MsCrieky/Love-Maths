9e1189c0415be229face10b00e7830fffbef19ec
MsCrieky <>
2023-10-11 11:40:40 +0000

    Add function to generate random number to  rungame

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,30 @@
+// Wait for the DOM to finish loading before running the game
+// Get the button elements and add event listeners to them
+
+document.addEventListener("DOMContentLoaded", function () {
+    let buttons = document.getElementsByTagName("button");
+
+    for (let button of buttons) {
+        button.addEventListener("click", function () {
+            if (this.getAttribute("data-type") === "submit") {
+                alert("You clicked Submit!");
+            } else {
+                let gameType = this.getAttribute("data-type");
+                alert(`You clicked ${gameType}`);
+            }
+        });
+    }
+});
+
 function rungame() {
+    let num1 = Math.floor(Math.random() * 25) + 1;
+    let num2 = Math.floor(Math.random() * 25) + 1;
 
 }
 
-function checkAnswer() (
+function checkAnswer() {
 
-)
+}
 
 function calculateCorrectAnswer() {
 
@@ -27,5 +47,5 @@ function displaySubtractQuestion() {
 }
 
 function displayMultiplyQuestion() {
-    
+
 }
